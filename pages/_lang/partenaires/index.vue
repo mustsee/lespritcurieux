ba9e6730a5295c8b2ffc3fe5d1037bb4c8b94fc6
Thomas <thomas.sypniewski@gmail.com>
2019-09-08 21:51:00 +0200

    add logos of partners

--- pages/_lang/partenaires/index.vue
+++ pages/_lang/partenaires/index.vue
@@ -1,21 +1,53 @@
 <template>
   <section>
     <div class="image">
-      <img class="bigger" src="/UW.jpg" alt="Logo de l'Université de Varsovie" />
+      <img class="bigger" src="/partners/senat-polski.jpg" alt="Logo du sénat Polonais" />
+    </div>    
+    <div class="image senat">
+      <div>{{ $t('partners.senat') }}</div>
+    </div> 
+    <div class="image">
+      <img class="bigger" src="/partners/pan.png" alt="Logo de l'académie polonaise des sciences" />
     </div>
     <div class="image">
-      <img class="bigger" src="/BUW.jpg" alt="Logo de la bibliothèque de l'Université de Varsovie" />
+      <img class="extra-large" src="/partners/pzu.jpg" alt="Logo de PZU" />
     </div>
     <div class="image">
-      <img src="/MANGGHA.jpg" alt="Logo of Manggha. Museum of Japanese Art and Technology" />
+      <img class="bigger" src="/partners/iam.jpg" alt="Logo de l'institut Adam Mickiewicz" />
     </div>
     <div class="image">
-      <img src="/Université-de-Poitiers.jpg" alt="Logo de l'Université de Poitiers" />
+      <img src="/partners/inVisu.jpg" alt="Logo de inVisu" />
     </div>
     <div class="image">
-      <img src="/inVisu.jpg" alt="Logo de inVisu" />
+      <img src="/partners/le-gallerie-degli-uffizi.svg" alt="Logo de la gallerie des Offices" />
     </div>
-    <div class="image"></div>
+    <div class="image">
+      <img class="bigger" src="/partners/UW.jpg" alt="Logo de l'Université de Varsovie" />
+    </div>
+    <div class="image">
+      <img class="bigger" src="/partners/BUW.jpg" alt="Logo de la bibliothèque de l'Université de Varsovie" />
+    </div>
+    <div class="image">
+      <img class="bigger" src="/partners/dida.jpg" alt="Logo de la bibliothèque de l'Université de Florence, département d'architecture" />
+    </div>
+    <div class="image">
+      <img src="/partners/Université-de-Poitiers.jpg" alt="Logo de l'Université de Poitiers" />
+    </div>
+    <div class="image">
+      <img  class="smaller" src="/partners/artes-liberales.png" alt="Logo de la faculté des Arts libéraux de Varsovie" />
+    </div>
+    <div class="image">
+      <img src="/partners/MANGGHA.jpg" alt="Logo of Manggha. Museum of Japanese Art and Technology" />
+    </div>
+    <div class="image">
+      <img class="bigger" src="/partners/e-campus.jpg" alt="Logo de l'Université de Côme">
+    </div>
+    <div class="image">
+      <img class="smaller" src="/partners/instytut-romanistyki.svg" alt="Logo de l'institut de romanistique">
+    </div>
+    <div class="image">
+      <img src="/partners/université-de-bologne.jpg" alt="Logo de l'Université de Bologne">
+    </div>   
   </section>
 </template>
 
@@ -46,8 +78,14 @@ section {
       width: 70%;
       max-height: auto;
     }
+    .smaller {
+      width: 50%;
+    }
     .bigger {
       width: 90%;
+    }
+    .extra-large {
+      width: 100%;
     }
   }
 }
